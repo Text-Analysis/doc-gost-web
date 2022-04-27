c589934ac9aaa93334fa5fd064d275552193a33b
BossMouseFire <abanin-02@mail.ru>
2022-04-27 12:58:24 +0400

    edit path of api

--- src/services/documentService.ts
+++ src/services/documentService.ts
@@ -10,59 +10,57 @@ import { IData, IDocumentFull } from '../types/actions/document';
 
 class DocumentService extends Api {
     public getDocuments() {
-        return this.get<RequestDocuments>('/specifications');
+        return this.get<RequestDocuments>('/specification/');
     }
 
     public getDocument(id: string) {
-        return this.get<IDocumentFull>(`/specifications/${id}`);
+        return this.get<IDocumentFull>(`/specification/${id}`);
     }
 
     public updateDocument(id: string, obj: IData[]) {
-        return this.put<string, IUpdateDocumentProps>(`/specifications/${id}`, {
+        return this.put<string, IUpdateDocumentProps>(`/specification/${id}`, {
             structure: obj,
         });
     }
 
     public createDocument(name: string, obj: IData[]) {
-        return this.post<string, ICreateDocumentProps>('/specifications/', {
+        return this.post<string, ICreateDocumentProps>('/specification/', {
             name: name,
             structure: obj,
         });
     }
 
     public getTemplate(id: string) {
-        return this.get<IDocumentFull>(`/templates/${id}`);
+        return this.get<IDocumentFull>(`/template/${id}`);
     }
 
     public parseDocument(file: File) {
         const formData = new FormData();
         formData.set('file', file);
-        return this.post<IData[], FormData>('/file', formData);
+        return this.post<IData[], FormData>('/file/', formData);
     }
 
     public getSections(id: string) {
-        return this.get<string[]>(`sections/${id}`);
+        return this.get<string[]>(`section/${id}`);
     }
 
     public getKeywords(id: string, mode: Mode, section = '') {
         if (mode === 'combine' || mode === 'tf_idf') {
             if (section) {
                 return this.get<IKeywordsTypeOne>(
-                    `/specifications/${id}/keywords?mode=${mode}&section=${section}`
+                    `/specification/${id}/keywords?mode=${mode}&section=${section}`
                 );
             }
             return this.get<IKeywordsTypeOne>(
-                `/specifications/${id}/keywords?mode=${mode}`
+                `/specification/${id}/keywords?mode=${mode}`
             );
         }
         if (section) {
             return this.get<string[]>(
-                `/specifications/${id}/keywords?mode=${mode}&section=${section}`
+                `/specification/${id}/keywords?mode=${mode}&section=${section}`
             );
         }
-        return this.get<string[]>(
-            `/specifications/${id}/keywords?mode=${mode}`
-        );
+        return this.get<string[]>(`/specification/${id}/keywords?mode=${mode}`);
     }
 }
 
